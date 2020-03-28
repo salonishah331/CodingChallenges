6c9c2ed64c7fb20c6c584df74c42e777348e8498
salonishah331 <51150161+salonishah331@users.noreply.github.com>
2020-03-28 16:48:14 -0400

    Update SingleRiffle.py

--- Leetcode/SingleRiffle.py
+++ Leetcode/SingleRiffle.py
@@ -3,28 +3,28 @@
 #     Input: Half1: [1, 4, 5] Half2: [2, 3, 6] Deck: [1, 2, 3, 4, 5, 6]
 #     Output: True
 
-# def SingleRiffle(half1, half2, deck):
-#     ptr1 = 0 
-#     ptr2 = 0
-#     comparedeck = []
-#     for i in range(len(deck)):
-#         if half1[ptr1] < half2[ptr2]: 
-#             comparedeck.append(half1[ptr1])
-#             # half1.remove(half1[ptr1])
-#             if ptr1 != len(half1) -1:
-#                 ptr1 += 1
-#         elif half2[ptr2] < half1[ptr1]:
-#             comparedeck.append(half2[ptr2])
-#             # half2.remove(half2[ptr2])
-#             if ptr2 != len(half2) -1 :
-#                 ptr2 += 1
-#     print(comparedeck)
-#     print(ptr1)
-#     print(ptr2)
-#     if comparedeck == deck:
-#         return True
-#     else:
-#         return False 
+def SingleRiffle(half1, half2, deck):
+    ptr1 = 0 
+    ptr2 = 0
+    comparedeck = []
+    for i in range(len(deck)):
+        if half1[ptr1] < half2[ptr2]: 
+            comparedeck.append(half1[ptr1])
+            # half1.remove(half1[ptr1])
+            if ptr1 != len(half1) -1:
+                ptr1 += 1
+        elif half2[ptr2] < half1[ptr1]:
+            comparedeck.append(half2[ptr2])
+            # half2.remove(half2[ptr2])
+            if ptr2 != len(half2) -1 :
+                ptr2 += 1
+    print(comparedeck)
+    print(ptr1)
+    print(ptr2)
+    if comparedeck == deck:
+        return True
+    else:
+        return False 
 
 def SingleRiffle(half1, half2, deck):
     
