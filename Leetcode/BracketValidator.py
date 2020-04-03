aafb5bfde6cc718b97ffbba96281a9fff9cc77d4
salonishah331 <51150161+salonishah331@users.noreply.github.com>
2020-04-03 14:45:58 -0400

    Update BracketValidator.py

--- Leetcode/BracketValidator.py
+++ Leetcode/BracketValidator.py
@@ -4,9 +4,20 @@ Write a function to determine whether the openers and closers are properly neste
 Input: '{[]()}'
 Output: True
 '''
-def BracketValidator(string):
+def is_valid(string):
+    open_close = {'(' : ')','{' : '}','[' : ']'}
+    openers = set(open_close.keys())
+    closers = set(open_close.values())
+
     stack = []
-    for i in string: 
-        if string[i] == '{' or '[' or '(': 
-            stack.append(string[i])
-        elif 
+    for char in string:
+        if char in openers:
+            stack.append(char)
+        elif char in closers:
+            if not stack: # if the stack it empty it's false
+                return False
+            else:
+                if not open_close[stack.pop()] == char: # if the closer does not corren
+                    return False
+
+    return not stack
